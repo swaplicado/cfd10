11fecb599bd6d0f588362c366d643c774da27589
Sergio Flores <floresgtz@hotmail.com>
2018-09-11 11:08:20 -0500

    Implementation of methods notRequiredAccount...() into class cfd.ver33.DCfdi33Utils.

--- src/cfd/ver33/DCfdi33Utils.java
+++ src/cfd/ver33/DCfdi33Utils.java
@@ -17,4 +17,25 @@ public class DCfdi33Utils {
         Pattern pattern = Pattern.compile(regex);
         return pattern.matcher(text).matches();
     }
+    
+    /**
+     * Check SAT CFDI 3.3 Catalogs for reference.
+     * @param formaPago
+     * @return 
+     */
+    public static boolean notRequiredAccountPayer(final String formaPago) {
+        return
+                formaPago.equals("01") || formaPago.equals("08") || formaPago.equals("12") || formaPago.equals("13") || formaPago.equals("14") || 
+                formaPago.equals("15") || formaPago.equals("17") || formaPago.equals("23") || formaPago.equals("24") || formaPago.equals("25") || 
+                formaPago.equals("26") || formaPago.equals("27") || formaPago.equals("30") || formaPago.equals("31");
+    }
+    
+    /**
+     * Check SAT CFDI 3.3 Catalogs for reference.
+     * @param formaPago
+     * @return 
+     */
+    public static boolean notRequiredAccountPayerReceipt(final String formaPago) {
+        return notRequiredAccountPayer(formaPago) || formaPago.equals("06");
+    }
 }
