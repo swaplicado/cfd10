3f98abe6b40162144286b0fcf13b3fee2982aaf9
Sergio Flores <floresgtz@hotmail.com>
2018-11-06 12:17:33 -0600

    2018-11-06 Sergio Abraham Flores Gutiérrez -------------------------------------------------------------------------------- Reestructuración y definición de constantes para el nuevo esquema de cancelación de CFDI, vigente a partir del 01 noviembre 2018.

--- src/cfd/ver33/DCfdi33Consts.java
+++ src/cfd/ver33/DCfdi33Consts.java
@@ -23,4 +23,30 @@ public class DCfdi33Consts {
     public static final String REGEX_UUID = "[a-f0-9A-F]{8}-[a-f0-9A-F]{4}-[a-f0-9A-F]{4}-[a-f0-9A-F]{4}-[a-f0-9A-F]{12}";
     public static final String REGEX_NUM_PEDIMENTO = "[0-9]{2}  [0-9]{2}  [0-9]{4}  [0-9]{7}";
     public static final String REGEX_NUM_PREDIAL = "[0-9]{1,150}";
+
+    /*
+    Constantes para la gestión de la cancelación de CFDI.
+    El texto y caso mayúsculas y minúsculas están de acuerdo a los web services del SAT.
+    */
+    
+    public static final String CANCELABLE_SIN_ACEPT = "Cancelable sin aceptación";
+    public static final String CANCELABLE_CON_ACEPT = "Cancelable con aceptación";
+    public static final String CANCELABLE_NO = "No cancelable";
+    
+    public static final String CFDI_ESTATUS_VIG = "Vigente";
+    public static final String CFDI_ESTATUS_CAN = "Cancelado";
+    
+    public static final String ESTATUS_CANCEL_PROC = "En proceso";
+    public static final String ESTATUS_CANCEL_RECH = "Solicitud rechazada";
+    public static final String ESTATUS_CANCEL_SIN_ACEPT = "Cancelado sin aceptación";
+    public static final String ESTATUS_CANCEL_CON_ACEPT = "Cancelado con aceptación";
+    public static final String ESTATUS_CANCEL_PLAZO_VENC = "Cancelado Plazo Vencido";
+    public static final String ESTATUS_CANCEL_NINGUNO = "None";
+    
+    public static final String ESTATUS_CANCEL_PROC_GUI_CODE = "EP";
+    public static final String ESTATUS_CANCEL_RECH_GUI_CODE = "SR";
+    public static final String ESTATUS_CANCEL_SIN_ACEPT_GUI_CODE = "CSA";
+    public static final String ESTATUS_CANCEL_CON_ACEPT_GUI_CODE = "CCA";
+    public static final String ESTATUS_CANCEL_PLAZO_VENC_GUI_CODE = "CPV";
+    public static final String ESTATUS_CANCEL_NINGUNO_GUI_CODE = "";
 }
