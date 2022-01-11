b460ac5bea6be314488c1053364ff176eec55f66
Sergio Flores <sflores@swaplicado.com.mx>
2022-01-11 12:00:08 -0600

    Creación de constantes para el CFDI versión 4.

---
+++ src/cfd/ver4/DCfdi4Consts.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cfd.ver4;
+
+import java.util.HashMap;
+
+/**
+ *
+ * @author Sergio Flores
+ */
+public abstract class DCfdi4Consts {
+    
+    public static final String CAN_MOTIVO_ERROR_CON_REL = "01"; // Comprobante emitido con errores de relación
+    public static final String CAN_MOTIVO_ERROR_SIN_REL = "02"; // Comprobante emitido con errores sin relación
+    public static final String CAN_MOTIVO_OPERACION_CAN = "03"; // No se llevó acabo la operación
+    public static final String CAN_MOTIVO_FACTURA_GLOBAL = "04"; // Operación nominativa relacionada en una factura global
+    
+    public static final HashMap<String, String> CancelaciónMotivos = new HashMap<>();
+    
+    static {
+        CancelaciónMotivos.put(CAN_MOTIVO_ERROR_CON_REL, "Comprobante emitido con errores de relación");
+        CancelaciónMotivos.put(CAN_MOTIVO_ERROR_SIN_REL, "Comprobante emitido con errores sin relación");
+        CancelaciónMotivos.put(CAN_MOTIVO_OPERACION_CAN, "No se llevó acabo la operación");
+        CancelaciónMotivos.put(CAN_MOTIVO_FACTURA_GLOBAL, "Operación nominativa relacionada en una factura global");
+    }
+}
